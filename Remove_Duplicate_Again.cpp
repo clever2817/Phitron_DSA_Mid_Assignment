036dbb0efde606a006ae25682dc8bf0624dc775e
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-08-01 02:17:02 +0600

    Remove Duplicate Again problem solved using STL List

---
+++ Remove_Duplicate_Again.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    list<int> l;
+
+    int val;
+
+    while(true)
+    {
+        cin >> val;
+
+        if(val == -1)
+        {
+            break;
+        }
+
+        l.push_back(val);
+    }
+
+
+    l.sort();
+
+    l.unique();
+
+    for(auto it : l)
+    {
+        cout << it << " ";
+    }
+
+    cout << "\n";
+
+    return 0;
+}
